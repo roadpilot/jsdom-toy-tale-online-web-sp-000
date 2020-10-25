10e26739a6cda60b0a6851dfac37b6044bd4e0e8
mdacht777 <mdacht777@gmail.com>
2020-10-25 13:52:55 -0600

    commit tree c8acf014ea24e5b5ef35b6d6d3ba245dd3d80a28

--- src/index.js
+++ src/index.js
@@ -12,4 +12,49 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const fetchUrl = 'http://localhost:3000/toys'
+  fetch(fetchUrl)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+      // console.log(json)
+      toyCard(json)
+      // for (const key in json.message) {
+      //     // console.log(key);
+      //     showBreeds(key)
+  });
+
+  function toyCard(json){
+    // <div class="card">
+    // <h2>Woody</h2>
+    // <img src=toy_image_url class="toy-avatar" />
+    // <p>4 Likes </p>
+    // <button class="like-btn">Like <3</button>
+    // </div>
+    b = document.getElementById('toy-collection')
+    for (const key in json) {
+        // console.log(json[key].image);
+      a = document.createElement('div')
+      a.class = "card"
+      h2 = document.createElement('h2')
+      h2.textContent = json[key].name
+      p = document.createElement('p')
+      p.textContent = `${json[key].likes} likes`
+      btn = document.createElement('button')
+      btn.class = "like-btn"
+      btn.textContent = "Like <3"
+      c = document.createElement('img')
+      c.height = 100
+      c.width = 100
+      c.src = json[key].image
+      b.appendChild(a)
+      a.appendChild(h2)
+      a.appendChild(p)
+      a.appendChild(c)
+      a.appendChild(btn)
+    }
+  }
+
 });
